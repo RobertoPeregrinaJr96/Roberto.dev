8576c62fb1f88b48cef9f4053958ab974bb782f6
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-10-13 11:16:44 -0700

    outlining a idea of the structure

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="name-container">
+        <h1 className="name-legal">Roberto Peregrina Jr</h1>
+        <p className="name-preferd">Preferd name is Robert</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="project-container">
+        <p className="project-01">Project 1</p>
+        <p className="project-02">Project 2</p>
+        <p className="project-03">Project 3</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
